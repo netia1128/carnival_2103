5ca7bd2358feb71a7ad0d0ac09788db42a1bd883
Netia Ingram <netia1128@hotmail.com>
2021-04-15 09:19:31 -0600

    Add code for attendee #initialize and test for attendee #add_interests

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -3,7 +3,7 @@ require './lib/attendee'
 RSpec.describe Attendee do
 
     before do
-        @attendee = Attendee.new({'Bob', 20)
+        @attendee = Attendee.new('Bob', 20)
     end
 
     describe '#initialize' do
@@ -14,6 +14,13 @@ RSpec.describe Attendee do
             expect(@attendee.name).to eq('Bob')
             expect(@attendee.spending_money).to eq(20)
             expect(@attendee.interests).to eq([])
+        end
+    end
+    describe '#add_interests' do
+        it 'allows you to add interests' do
+            @attributes.add_interests('Bumper Cars')
+            @attributes.add_interests('Ferris Wheel')
+            expect(@attendee.interests).to eq(['Bumper Cars', 'Ferris Wheel'])
         end
     end
 end
