cc300e2af87daa81fbd5b72642d0a65fd6b8b012
Netia Ingram <netia1128@hotmail.com>
2021-04-15 09:51:43 -0600

    Add test for carnival #attendees_by_ride_interest

--- spec/carinival_spec.rb
+++ spec/carinival_spec.rb
@@ -58,4 +58,21 @@ RSpec.describe Carnival do
             expect(@jeffco_fair.attendees).to eq([@bob, @sally, @johnny])
         end
     end
+    describe '#attendees_by_ride_interest' do
+        it 'creates a hash to display attendees by ride interest' do
+            @jeffco_fair.add_ride(@ferris_wheel)
+            @jeffco_fair.add_ride(@bumper_cars)
+            @jeffco_fair.add_ride(@scrambler)
+            @bob.add_interest('Ferris Wheel')
+            @bob.add_interest('Bumper Cars')
+            @sally.add_interest('Scrambler')
+            @johnny.add_interest('Bumper Cars')
+            @jeffco_fair.admit(@bob)
+            @jeffco_fair.admit(@sally)
+            @jeffco_fair.admit(@johnny)
+            expect(@jeffco_fair.attendees_by_ride_interest).to eq({@ferris_wheel => [@bob], 
+                                                                   @bumper_cars => [@bob, @johnny],
+                                                                   @scrambler => [@sally]})
+        end
+    end
 end
