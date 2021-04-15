e1998cb4a613cfeb7cfdcf79ff58c31b91af747b
Netia Ingram <netia1128@hotmail.com>
2021-04-15 09:28:54 -0600

    Add code for carnival #initialize and test for carnival #add_ride

---
+++ lib/carnival.rb
@@ -0,0 +1,9 @@
+class Carnival
+    attr_reader :name,
+                :rides
+
+    def initialize(name)
+        @name = name
+        @rides = []
+    end
+end
--- spec/carinival_spec.rb
+++ spec/carinival_spec.rb
@@ -5,16 +5,27 @@ require './lib/carnival'
 RSpec.describe Carnival do
 
     before do
-        @carnival = carnival.new('Jefferson County Fair')
+        @jeffco_fair = Carnival.new('Jefferson County Fair')
+        @ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
+        @bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
+        @scrambler = Ride.new({name: 'Scrambler', cost: 15})
     end
 
     describe '#initialize' do
         it 'exists' do
-            expect(@carnival).to be_instance_of(Carnival)
+            expect(@jeffco_fair).to be_instance_of(Carnival)
         end
         it 'has attributes' do
-            expect(@carnival.name).to eq('Jefferson County Fair')
-            expect(@carnival.rides).to eq([])
+            expect(@jeffco_fair.name).to eq('Jefferson County Fair')
+            expect(@jeffco_fair.rides).to eq([])
+        end
+    end
+    describe '#add_ride' do
+        it 'allows you to add rides' do
+            @jeffco_fair.add_ride(@ferris_wheel)
+            @jeffco_fair.add_ride(@bumper_cars)
+            @jeffco_fair.add_ride(@scrambler)
+            expect(@jeffco_fair.rides).to eq([@ferris_wheel, @bumper_cars, @scrambler])
         end
     end
 end
